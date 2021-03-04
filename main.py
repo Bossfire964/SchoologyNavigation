d29c3eeb2a8f91f4fa3294404406aa8fbf803e6d
Bossfire964 <74617357+Bossfire964@users.noreply.github.com>
2021-03-03 22:51:39 -0500

    Added SignIn

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello")
+from selenium import webdriver
+import os
+import time
+import sys
+
+browser = webdriver.Chrome('/Users/luke/Documents/chromedriver')
+browser.get('https://lms.fcps.org')
+usernameBox = browser.find_elements_by_xpath('//*[@id="userNameInput"]')[0]
+usernameBox.send_keys(sys.argv[1])
+passwordBox = browser.find_elements_by_xpath('//*[@id="passwordInput"]')[0]
+passwordBox.send_keys(sys.argv[2])
+sign_in = browser.find_elements_by_xpath('//*[@id="submitButton"]')[0]
+sign_in.click()
+
